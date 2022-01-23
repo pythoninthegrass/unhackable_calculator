36354760a60a192ac037ee682ff9d880b8799b70
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-01-22 21:53:13 -0600

    Update unhackable.py
    Moved some of Logan's code into mine, added `requests_cache`, todo w/`re` library
    
    Co-Authored-By: sweetroll <29440697+sw33tr0ll@users.noreply.github.com>

--- unhackable.py
+++ unhackable.py
@@ -1,26 +1,29 @@
 # import lxml
 import pandas as pd
+import re
 import requests
+import requests_cache
 from bs4 import BeautifulSoup
 from icecream import ic
 
+base_url = 'https://app.cloud-logon.com/dev/'
+calc_url = base_url + "calculator"
+hint_url = base_url + "easy_mode"
 
-calc_url = 'https://app.cloud-logon.com/dev/calculator'
+requests_cache.install_cache('main_page_cache')
+main_page = requests.get(calc_url)
 
-page = requests.get(calc_url)
-ic(page.status_code)
-ic(page.text)
-
-soup = BeautifulSoup(page.text, 'html.parser')
-ic(soup.prettify())
+# TODO: extract AWS account number
+main_page_soup = BeautifulSoup(main_page.text, 'html.parser')
+print(f"MAIN PAGE\n\n{main_page_soup}")
+ic(main_page_soup.prettify())
 # print(soup.find_all('h1'))
 # print(soup.find_all('h1')[2].get_text())
 
 c = pd.read_csv(calc_url)
-# ic(c)
 print(c)
 
-url_hint = 'https://app.cloud-logon.com/dev/easy_mode'
-page = requests.get(url_hint)
-ic(page.text)
-
+hint_page = requests.get(hint_url)
+hint_page_soup = BeautifulSoup(hint_page.text, 'html.parser')
+print(f"HINT PAGE\n\n{hint_page_soup}")
+ic(hint_page.text)
