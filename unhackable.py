df01025e4ee6244e1640a6353c16fb835476e828
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-01-27 00:45:05 -0600

    Proxyman env vars
    Reads .env in working directory, then expands tilde/$HOME vars and converts to a string that requests can read

--- unhackable.py
+++ unhackable.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
 
-import boto3
-import botocore.session
-import logging
+# import boto3
+# import botocore.session
+# import logging
+import os
 import pandas as pd
 import re
 import requests
 import requests_cache
 from bs4 import BeautifulSoup, Comment
 from datetime import timedelta
+from decouple import config
 # from icecream import ic
 from pathlib import Path
 
 base_url = 'https://app.cloud-logon.com/dev/'
 calc_url = base_url + "calculator"
 hint_url = base_url + "easy_mode"
+
+# proxyman certs
+env = Path('.env')
+if env.exists():
+    SSL_CERT_FILE = config('SSL_CERT_FILE')
+    REQUESTS_CA_BUNDLE = config('REQUESTS_CA_BUNDLE')
+
+    SSL_CERT_FILE = (Path(SSL_CERT_FILE)).expanduser()
+    REQUESTS_CA_BUNDLE = (Path(REQUESTS_CA_BUNDLE)).expanduser()
+
+    os.environ['SSL_CERT_FILE'] = str(SSL_CERT_FILE)
+    os.environ['REQUESTS_CA_BUNDLE'] = str(REQUESTS_CA_BUNDLE)
 
 requests_cache.install_cache("pages_cache")
 main_page = requests.get(calc_url)
@@ -23,7 +37,7 @@ page_soup = BeautifulSoup(main_page.text, 'html.parser')
 # print(f"MAIN PAGE\n{page_soup}")
 
 comments = page_soup.find_all(string=lambda text: isinstance(text, Comment))
-print("COMMENTS")
+print("\nCOMMENTS\n")
 for comment in comments:
     print(comment.strip())
 
