619d11511559804b6b8a3312604bb580fbbcc7e6
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-01-26 23:54:19 -0600

    Update unhackable.py
    Simplified cache, commented out duplicate stdout

--- unhackable.py
+++ unhackable.py
@@ -1,31 +1,26 @@
+#!/usr/bin/env python3
+
+import boto3
+import botocore.session
+import logging
 import pandas as pd
 import re
 import requests
+import requests_cache
 from bs4 import BeautifulSoup, Comment
 from datetime import timedelta
 # from icecream import ic
 from pathlib import Path
-from requests_cache import CachedSession
 
 base_url = 'https://app.cloud-logon.com/dev/'
 calc_url = base_url + "calculator"
 hint_url = base_url + "easy_mode"
 
-session = CachedSession(
-    'pages_cache',
-    use_cache_dir=False,               # Save files in the default user cache dir
-    cache_control=True,                # Use Cache-Control headers for expiration, if available
-    expire_after=timedelta(days=1),    # Otherwise expire responses after one day
-    allowable_methods=['GET', 'POST'], # Cache POST requests to avoid sending the same data twice
-    allowable_codes=[200, 400],        # Cache 400 responses as a solemn reminder of your failures
-    ignored_parameters=['api_key'],    # Don't match this param or save it in the cache
-    match_headers=True,                # Match all request headers
-    stale_if_error=True,               # In case of request errors, use stale cache data if possible
-)
+requests_cache.install_cache("pages_cache")
 main_page = requests.get(calc_url)
 
 page_soup = BeautifulSoup(main_page.text, 'html.parser')
-print(f"MAIN PAGE\n{page_soup}")
+# print(f"MAIN PAGE\n{page_soup}")
 
 comments = page_soup.find_all(string=lambda text: isinstance(text, Comment))
 print("COMMENTS")
